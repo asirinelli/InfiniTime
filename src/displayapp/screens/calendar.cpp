3c3e7dc7d7d19b2bcc9cbcb1cb0cb81eae2fb061
ZephyrLabs <65145081+ZephyrLabs@users.noreply.github.com>
2021-02-10 21:02:00 +0530

    Update calendar.cpp

--- src/displayapp/screens/calendar.cpp
+++ src/displayapp/screens/calendar.cpp
@@ -4,11 +4,37 @@
 
 using namespace Pinetime::Applications::Screens;
 
-calendar::calendar(Pinetime::Applications::DisplayApp* app, Pinetime::Components::LittleVgl& lvgl) : Screen(app), lvgl{lvgl} {
-  app->SetTouchMode(DisplayApp::TouchModes::Polling);
+static void event_handler(lv_obj_t * obj, lv_event_t event)
+{
+    if(event == LV_EVENT_VALUE_CHANGED) {
+        lv_calendar_date_t * date = lv_calendar_get_pressed_date(obj);
+        }
+    }
 }
 
-calendar::calendar() {
+calendar::calendar(Pinetime::Applications::DisplayApp* app, Pinetime::Components::LittleVgl& lvgl) : Screen(app), lvgl{lvgl} {
+  app->SetTouchMode(DisplayApp::TouchModes::Polling);
+
+    lv_obj_t  * calendar = lv_calendar_create(lv_scr_act(), NULL);
+    lv_obj_set_size(calendar, 235, 235);
+    lv_obj_align(calendar, NULL, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_set_event_cb(calendar, event_handler);
+
+    /*Make the date number smaller to be sure they fit into their area*/
+    lv_obj_set_style_local_text_font(calendar, LV_CALENDAR_PART_DATE, LV_STATE_DEFAULT, lv_theme_get_font_small());
+
+    /*Set today's date*/
+    lv_calendar_date_t today;
+    today.year = 2021;
+    today.month = 2;
+    today.day = 10;
+
+    lv_calendar_set_today_date(calendar, &today);
+    lv_calendar_set_showed_date(calendar, &today);
+
+}
+
+calendar::~calendar() {
   // Reset the touchmode
   app->SetTouchMode(DisplayApp::TouchModes::Gestures);
   lv_obj_clean(lv_scr_act());
