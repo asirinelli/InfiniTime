01a7e22cc00b9476c51818f1e557f30f0f66d6a4
ZephyrLabs <65145081+ZephyrLabs@users.noreply.github.com>
2021-02-11 16:29:56 +0530

    Update calendar.cpp

--- src/displayapp/screens/calendar.cpp
+++ src/displayapp/screens/calendar.cpp
@@ -4,32 +4,42 @@
 
 using namespace Pinetime::Applications::Screens;
 
-static void event_handler(lv_obj_t * obj, lv_event_t event)
+static void event_handler(lv_obj_t* obj, lv_event_t event)
 {
-    if(event == LV_EVENT_VALUE_CHANGED) {
-        lv_calendar_date_t * date = lv_calendar_get_pressed_date(obj);
-    }
+  calendar* screen = static_cast<calendar *>(obj->user_data);
+  screen->OnObjectEvent(obj, event);
 }
 
 calendar::calendar(Pinetime::Applications::DisplayApp* app, Pinetime::Components::LittleVgl& lvgl) : Screen(app), lvgl{lvgl} {
   app->SetTouchMode(DisplayApp::TouchModes::Polling);
 
-    lv_obj_t  * calendar = lv_calendar_create(lv_scr_act(), NULL);
-    lv_obj_set_size(calendar, 235, 235);
-    lv_obj_align(calendar, NULL, LV_ALIGN_CENTER, 0, 0);
-    lv_obj_set_event_cb(calendar, event_handler);
+  
 
-    /*Make the date number smaller to be sure they fit into their area*/
-    lv_obj_set_style_local_text_font(calendar, LV_CALENDAR_PART_DATE, LV_STATE_DEFAULT, lv_theme_get_font_small());
+  lv_obj_t* calendar = lv_calendar_create(lv_scr_act(), NULL);
+  lv_obj_set_size(calendar, 235, 235);
+  lv_obj_align(calendar, NULL, LV_ALIGN_CENTER, 0, 0);
 
-    /*Set today's date*/
-    lv_calendar_date_t today;
-    today.year = 2021;
-    today.month = 2;
-    today.day = 10;
+  static lv_style_t style;
+  lv_style_copy(&style,lv_calendar_get_style(calendar,LV_CALENDAR_STYLE_DAY_NAMES));
+  style.text.font = &jetbrains_mono_bold_20;
+  lv_calendar_set_style(calendar, LV_CALENDAR_STYLE_DAY_NAMES, &style);
+  lv_calendar_set_style(calendar, LV_CALENDAR_STYLE_HEADER, &style);
+  lv_calendar_set_style(calendar, LV_CALENDAR_STYLE_HEADER_PR, &style);
+  lv_calendar_set_style(calendar, LV_CALENDAR_STYLE_BG, &style);
 
-    lv_calendar_set_today_date(calendar, &today);
-    lv_calendar_set_showed_date(calendar, &today);
+  lv_obj_set_event_cb(calendar, event_handler);
+
+  /*Make the date number smaller to be sure they fit into their area*/
+  lv_obj_set_style_local_text_font(calendar, LV_CALENDAR_PART_DATE, LV_STATE_DEFAULT, lv_theme_get_font_small());
+
+  /*Set today's date*/
+  lv_calendar_date_t today;
+  today.year = 2021;
+  today.month = 2;
+  today.day = 11;
+
+  lv_calendar_set_today_date(calendar, &today);
+  lv_calendar_set_showed_date(calendar, &today);
 
 }
 
